3c3da19025f0c328c77f69174457f2e37beb114c
Jonathan Brumley <cayasso@gmail.com>
2013-09-11 23:53:34 -0600

    Fixing tests

--- test/test.js
+++ test/test.js
@@ -2,43 +2,57 @@ var mongooseCachebox = require('../');
 var mongoose = require('mongoose');
 var expect = require('expect.js');
 var Schema = mongoose.Schema;
+var PeopleSchema;
+var People;
+var db;
 
 var names = ["Jacob", "Sophia", "Mason", "Isabella", "William", "Emma", "Jayden", "Olivia", "Noah", "Ava", "Michael", "Emily", "Ethan", "Abigail", "Alexander", "Madison", "Aiden", "Mia", "Daniel", "Chloe"];
 
-// connecting to mongoose
-mongoose.connect('mongodb://127.0.0.1/mongoose-cachebox-testing');
-
-// adding mongoose cachebox
-mongooseCachebox(mongoose, { engine: 'memory' });
-
-var PeopleSchema = new Schema({
-  name: String,
-  date: {
-    type: String,
-    "default": Date.now()
-  },
-  num: Number,
-  test: Boolean
-});
-
-var People = mongoose.model('People', PeopleSchema);
-
-function generate (amount, fn) {
-  var crowd = [];
-  var count = 0;
-  while (count < amount) {
-    crowd.push({
-      name: names[Math.floor(Math.random() * names.length)],
-      num: Math.random() * 10000
-    });
-    count++;
-  }
-  People.create(crowd, fn);
-}
-
-//generate(10, function () {});
-
 describe('mongooseCachebox', function () {
+
+
+  before(function (done) {
+    // connecting to mongoose
+    mongoose.connect('mongodb://127.0.0.1/mongoose-cachebox-testing');
+
+    db = mongoose.connection;
+    
+    db.on('error', function (err) {
+      done(err);
+    });
+
+    db.once('open', done);
+
+    // adding mongoose cachebox
+    mongooseCachebox(mongoose, { engine: 'memory' });
+
+    PeopleSchema = new Schema({
+      name: String,
+      date: {
+        type: String,
+        "default": Date.now()
+      },
+      num: Number,
+      test: Boolean
+    });
+
+    People = mongoose.model('People', PeopleSchema);
+  });  
+
+  function generate (amount, fn) {
+    var crowd = [];
+    var count = 0;
+    while (count < amount) {
+      crowd.push({
+        name: names[Math.floor(Math.random() * names.length)],
+        num: Math.random() * 10000
+      });
+      count++;
+    }
+    People.create(crowd, fn);
+  }
+
+
 
   beforeEach(function(done){
     generate(10, done);
